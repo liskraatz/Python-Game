8b680528a42a61014b2943bbb19713b4802589c5
Lisa Kraatz <liskraatz@gmail.com>
2026-05-11 13:38:48 -0400

    Added player image and event handling for left and right movement witth arrow keys

--- game.py
+++ game.py
@@ -20,17 +20,46 @@ pygame.display.set_caption('Name of game in progress')
 # Game Clock (fps)
 clock = pygame.time.Clock()
 
+# Player Character Image
+playerImg = pygame.image.load('player.png')
+
+def player(x,y):
+  gameDisplay.blit(playerImg, (x,y)) # Bliting player image to coordinates 
+
+x = (displayWidth * 0.45) # Middle of screen (Player image is referenced from top left corner)
+y = (displayHeight * 0.7) # Bottom of screen
+
+xChange = 0
+
+
+
 # Initiate variables
 crashed = False
 
-# Loop 
+# Event Handling Loop
 while not crashed:
   for event in pygame.event.get(): # (Get all events and put them in list)
-    if (event.type == pygame.QUIT): # If player clicks x (exits window)
+    # Player exists window
+    if (event.type == pygame.QUIT):
       crashed = True # Ends loop (for now)
+    # Key is pressed
+    if (event.type == pygame.KEYDOWN): 
+      if (event.key == pygame.K_LEFT): # Left Arrow key
+        xChange = -5
+      elif (event.key == pygame.K_RIGHT): # Right Arrow Key
+        xChange = 5
+    # Key Release
+    if (event.type == pygame.KEYUP):
+      if (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
+        xChange = 0
 
-  pygame.display.update() # Updates screen
-  clock.tick(30) # In fps 
+  x += xChange # 
+
+
+  gameDisplay.fill(white) # Colorize screen
+  player(x,y) # Show player
+  pygame.display.update() # Update screen
+  clock.tick(60) # In fps 
 
 # Quit
 pygame.quit() # Kills game
